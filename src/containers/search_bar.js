3926672b5bd88c054d0cac18a096151207684ef5
Reid Blomquist <reid@theprogrampdx.com>
2016-05-08 23:06:24 -0700

    such work much wow

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -10,6 +10,7 @@ class SearchBar extends Component {
     this.state = {term: ''};
     
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   
   onInputChange(event) {
@@ -20,6 +21,8 @@ class SearchBar extends Component {
     event.preventDefault();
     
     // fetch some weathery thangs
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
   
   render() {
@@ -40,7 +43,7 @@ class SearchBar extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({fetchWeather}. dispatch);
+  return bindActionCreators({fetchWeather}, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(SearchBar);
