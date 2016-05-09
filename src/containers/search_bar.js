f5810888a54b82870637ae8052291177c0284015
Reid Blomquist <reid@theprogrampdx.com>
2016-05-08 22:36:49 -0700

    turn dis into a container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     
@@ -38,3 +38,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}. dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
