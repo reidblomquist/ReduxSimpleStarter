a09dafb35e2457a3910611f0c4c9e1c16c216f55
Reid Blomquist <reid@theprogrampdx.com>
2016-05-08 22:35:38 -0700

    work work

---
+++ src/containers/search_bar.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {term: ''};
+    
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  
+  onInputChange(event) {
+    this.setState({term: event.target.value})
+  }
+  
+  onFormSubmit(event) {
+    event.preventDefault();
+    
+    // fetch some weathery thangs
+  }
+  
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input 
+          placeholder="Get a five-day forecast in your favorite shitties"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
