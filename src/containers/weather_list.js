2b3be908732ffc8af6016ba8c65a247726e3c756
Reid Blomquist <reid@theprogrampdx.com>
2016-05-10 08:49:48 -0700

    sparkylinez

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
+
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange"></Chart>
+        </td>
+      </tr>
+    );
+  }
+  
   render() {
     return (
       <table className="table table-hover">
@@ -14,7 +30,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-        
+        {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
