f55b7f43650f133057296818c3284e9581935198
Reid Blomquist <reid@theprogrampdx.com>
2016-05-12 17:30:19 -0700

    woo posts n stuffs

---
+++ src/components/posts_new.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {createPost} from '../actions/index';
+
+class PostsNew extends Component {
+  render () {
+    const {fields: {title, categories, content}, handleSubmit} = this.props;
+    
+    return (
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title}/>
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content}/>
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
+        </div>
+        
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+  
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+  
+  if (!values.categories) {
+    errors.categories = 'Enter at least one category';
+  }  
+  
+  if (!values.content) {
+    errors.content = 'Enter some content yo';
+  }    
+  
+  return errors;
+}
+
+// connect: first agument is mapStateToProps, 2nd is mapDispatchTo Props
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content'],
+  validate
+}, null, {createPost})(PostsNew);
